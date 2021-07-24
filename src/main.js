2dbcf1d59d41b9fc184f61efbdad0d3004c5e6fb
Mizael Pereira <mizael.pereira@optimalenergy.com.br>
2021-07-24 16:40:28 -0300

    add play audio

--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
 import notes from './notes.js';
 
-const getMIDIMessage = midiMessage => {
-    console.log(`You press a ${notes[midiMessage.data[1]][0]} note. The frequency is ${notes[midiMessage.data[1]][1]}`)
-}
+document.querySelector("button").addEventListener("click", () => {
 
+    const getMIDIMessage = midiMessage => {
 
+        const { data } = midiMessage
 
-navigator.requestMIDIAccess()
-    .then(midiAccess => {
+        const command = data[0]
+        const note = data[1]
+        const velocity = (data.length > 2) ? data[2] : 0
 
-        for(const input of midiAccess.inputs.values()) {
-            input.onmidimessage = getMIDIMessage
+        if(command === 144) {
+            playNote(notes[note][1])
         }
-    }, () => {
-        console.log('Could not access your MIDI devices')
-    })
+    
+        console.log(`You press a ${notes[note][0]} note. The frequency is ${notes[note][1]}`)
+    }
+    
+    navigator.requestMIDIAccess()
+        .then(midiAccess => {
+    
+            for(const input of midiAccess.inputs.values()) {
+                input.onmidimessage = getMIDIMessage
+            }
+            
+        }, () => {
+            console.log('Could not access your MIDI devices')
+        })
+    
+    const playNote = key => {
 
-// Future implementation
-const playNote = key => {
-//     const context = new AudioContext()
-//     const oscillator = context.createOscillator()
-//     oscillator.frequency.value = key
-//     oscillator.type = 'sine'
-//     oscillator.connect(context.destination)
+        const context = new AudioContext()
+        const oscillator = context.createOscillator()
 
-//     oscillator.start(context.currentTime)
-//     setTimeout(() => {
-//         oscillator.stop(context.currentTime)
-//     }, 500)
-}
+        oscillator.frequency.value = key
+        oscillator.type = 'sine'
+        oscillator.connect(context.destination)
+    
+        oscillator.start(context.currentTime)
+        setTimeout(() => {
+            oscillator.stop(context.currentTime)
+        }, 500)
+        context.resume()
+    }
+
+})
